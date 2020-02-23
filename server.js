7c0fd27dac8870ec7c70dbf348543176eddad89f
Krizel Minnema <krizelminnema@gmail.com>
2020-02-22 21:32:45 -0800

    adding handlebars connection

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // dependencies
-const express = require("express");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const axios = require('axios');
+const cheerio = require ('cheerio');
+
+// models
+const db = require('./models');
 
 // port
 const PORT = process.env.PORT || 3000;
@@ -11,12 +18,18 @@ var app = express();
 const router = express.Router();
 
 // static directory
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+
+// handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main', }),
+);
 
 // router middleware
 app.use(router);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // listen
 app.listen(PORT, function() {
-    console.log("Listening on http://localhost:" + PORT);
+    console.log('Listening on http://localhost:' + PORT);
 });
