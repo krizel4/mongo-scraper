92be8d50a24c65a1c1ec41c7fde1e5d9a815ee9f
Krizel Minnema <krizelminnema@gmail.com>
2020-02-22 21:14:41 -0800

    creating server

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // dependencies
+const express = require("express");
 
 // port
+const PORT = process.env.PORT || 3000;
 
-// initiate express
+// express app
+var app = express();
 
 // router
+const router = express.router();
 
 // static directory
+app.use(express.static(__dirname + "/public"));
 
 // router middleware
+app.use(router);
 
 // listen
+app.listen(PORT, function() {
+    console.log("Listening on http://localhost:" + PORT);
+});
