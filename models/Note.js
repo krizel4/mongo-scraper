970d7e9fdbf8443d6065844319a3f39ef196c8b8
Krizel Minnema <krizelminnema@gmail.com>
2020-02-23 16:33:12 -0800

    adding note model

---
+++ models/Note.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    // create unique article id to have notes associated with specific IDs
+    _headlineId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Headline'
+    },
+    date: String,
+    noteText: String
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Note;
