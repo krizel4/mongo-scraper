429959549b655600a3a09bd4379919db08e3b686
Krizel Minnema <krizelminnema@gmail.com>
2020-02-23 16:17:22 -0800

    finally connected, removed depracated engine and passed diff opt to mongo constructor

--- server.js
+++ server.js
@@ -2,41 +2,49 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+
+// scraping dependencies
 const axios = require('axios');
-const cheerio = require ('cheerio');
+const cheerio = require('cheerio');
+
 
 // port
 const PORT = process.env.PORT || 3000;
 
 // express app
-var app = express();
+const app = express();
 
 // router
 const router = express.Router();
+
+// routes
+require('./config/routes')(router);
 
 // static directory
 app.use(express.static(__dirname + '/public'));
 
 // handlebars
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 // router middleware
 // app.use(router);
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-// mongodb when deployed
+// // mongodb when deployed
 const db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(db, function(error) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("mongoose connection is successful");
-    }
-})
+mongoose.connect(db, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log(`Connected to ${db}`))
+    .catch((err) => console.error(err));
 
 // listen
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('Listening on http://localhost:' + PORT);
 });
